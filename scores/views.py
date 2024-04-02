5222385056cc498399b3b3d1dcece5329a29e87d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-02 23:21:10 +0200

    wip on fixtures

--- scores/views.py
+++ scores/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from scores.models import Fixture
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+
+def fixtures(request):
+    fixtures = Fixture.objects.all()
+    context = {
+        'fixtures': fixtures
+
+    }
+    return render(request, 'partials/fixtures.html', context)
